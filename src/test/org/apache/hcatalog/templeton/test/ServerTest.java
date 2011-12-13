b3c622ee2918e2d7d102926d0b0423f18df51bff
Rachel Gollub <rachel@gollub.net>
2011-12-12 21:41:25 -0800

    Cleaned up a little to check in test code.

---
+++ src/test/org/apache/hcatalog/templeton/test/ServerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hcatalog.templeton.test;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.hcatalog.templeton.ExecBean;
+import org.apache.hcatalog.templeton.SimpleWebException;
+
+/*
+ * Test that the server code exists.
+ */
+public class ServerTest extends TestCase {
+	
+	TestServer server;
+	
+	public void setUp() {
+		server = new TestServer();
+	}
+
+    public void testServer() {
+    	assertNotNull(server);
+    }
+    
+    public void testStatus() {
+    	assertTrue(server.status().indexOf("\"ok\"") > -1);
+    }
+    
+    public void testDDL() {
+    	try {
+    		validateDDL(server, "show tables", "admin", "777");
+    		validateDDL(server, "", "", "");
+    		validateDDL(server, "show tables", "", "");
+    		validateDDL(server, "show tables", "admin", "");
+    	} catch (SimpleWebException swe) {
+    		swe.printStackTrace();
+    		fail("ddl execution caused a failure");
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		fail("ddl execution caused a failure");
+    	}
+    }
+    
+    // More tests coming later.
+    
+    public void validateDDL(TestServer server, String command, String group, String permissions) 
+    	throws SimpleWebException, IOException {
+    	ExecBean bean = server.ddl(command, group, permissions);
+		assertTrue(bean.stdout.endsWith("bin/hcat"));
+		String tmp = bean.stderr.substring(bean.stderr.indexOf("[") + 1, bean.stderr.indexOf("]"));
+		String[] parts = tmp.split(",");
+		assertTrue(parts[0].trim().equals("-e"));
+		assertTrue(parts[1].trim().equals(command));
+		assertTrue(parts[2].trim().equals("-g"));
+		assertTrue(parts[3].trim().equals(group));
+		assertTrue(parts[4].trim().equals("-p"));
+		assertTrue(parts[5].trim().equals(permissions));
+    }
+}
---
+++ src/test/org/apache/hcatalog/templeton/test/TestExecService.java
@@ -0,0 +1,31 @@
+package org.apache.hcatalog.templeton.test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.exec.ExecuteException;
+import org.apache.hcatalog.templeton.ExecBean;
+import org.apache.hcatalog.templeton.ExecService;
+import org.apache.hcatalog.templeton.NotAuthorizedException;
+
+public class TestExecService implements ExecService {
+	
+	public ExecBean run(String user, String program, List<String> args,
+            Map<String, String> env) {
+		ExecBean bean = new ExecBean();
+		bean.stdout = program;
+		bean.stderr = args.toString();
+		return bean;
+	}
+
+	@Override
+	public ExecBean runUnlimited(String user, String program,
+			List<String> args, Map<String, String> env)
+			throws NotAuthorizedException, ExecuteException, IOException {
+		ExecBean bean = new ExecBean();
+		bean.stdout = program;
+		bean.stderr = args.toString();
+		return null;
+	}
+}
