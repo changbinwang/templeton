873dc02e526c52ef2a1cb723237b47d790eba5f9
Rachel Gollub <rachel@gollub.net>
2012-03-29 09:10:03 -0700

    Detect a semantic analysis error for describe database -- we should only get that error if the database doesn't exist, so we can return a better error message.

--- src/java/org/apache/hcatalog/templeton/HcatDelegator.java
+++ src/java/org/apache/hcatalog/templeton/HcatDelegator.java
@@ -92,6 +92,12 @@ public class HcatDelegator extends LauncherDelegator {
             String res = jsonRun(user, exec);
             return JsonBuilder.create(res).build();
         } catch (HcatException e) {
+            if (e.execBean.stderr.indexOf("Error in semantic analysis") > -1) {
+                return JsonBuilder.create().
+                        put("error", "Database " + db + " does not exist")
+                        .put("errorCode", "404")
+                        .put("database", db).build();
+            }
             throw new HcatException("unable to describe database: " + db,
                                     e.execBean, exec);
         }
