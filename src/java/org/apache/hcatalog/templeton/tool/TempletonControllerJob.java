f43d1bef5a20657bf1e109444ecc1262d5af675e
Thejas Nair <thejas@hortonworks.com>
2012-05-21 14:23:16 -0700

    Revert "Always exit the controller job with status 0.  Fixes #44"
    This reverts commit 6ba0ee30b5aa008c5612a4f1085b6b026dc1a9a0.
    (revert until #64 is fixed)

--- src/java/org/apache/hcatalog/templeton/tool/TempletonControllerJob.java
+++ src/java/org/apache/hcatalog/templeton/tool/TempletonControllerJob.java
@@ -160,11 +160,12 @@ public class TempletonControllerJob extends Configured implements Tool {
             state.setCompleteStatus("done");
             state.close();
 
-            if (proc.exitValue() != 0)
+            if (proc.exitValue() != 0) {
                 System.err.println("templeton: job failed with exit code "
                                    + proc.exitValue());
-            else
-                System.err.println("templeton: job completed with exit code 0");
+                System.exit(proc.exitValue());
+            }
+            System.err.println("templeton: job completed with exit code 0");
         }
 
         private void executeWatcher(ExecutorService pool, Configuration conf,
